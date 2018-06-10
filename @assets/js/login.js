80ec336fe340145fc045d290e1a84aebb6f23e4e
hashshura <hashshura@gmail.com>
2018-06-11 03:31:24 +0700

    ready to deploy

--- @assets/js/login.js
+++ @assets/js/login.js
@@ -3,16 +3,12 @@ $(document)
       $('.ui.form')
         .form({
           fields: {
-            email: {
-              identifier  : 'email',
-              rules: [
+            username: {
+              identifier  : 'username',
+              rules : [
                 {
-                  type   : 'empty',
-                  prompt : 'Please enter your e-mail'
-                },
-                {
-                  type   : 'email',
-                  prompt : 'Please enter a valid e-mail'
+                  type   : 'length[3]',
+                  prompt : 'Username must be at least 3 characters'
                 }
               ]
             },
@@ -20,12 +16,8 @@ $(document)
               identifier  : 'password',
               rules: [
                 {
-                  type   : 'empty',
-                  prompt : 'Please enter your password'
-                },
-                {
-                  type   : 'length[6]',
-                  prompt : 'Your password must be at least 6 characters'
+                  type   : 'length[8]',
+                  prompt : 'Password must be at least 8 characters'
                 }
               ]
             }
--- @assets/js/register.js
+++ @assets/js/register.js
@@ -3,29 +3,48 @@ $(document)
       $('.ui.form')
         .form({
           fields: {
+            firstname: {
+              identifier : 'firstname',
+              rules : [
+                {
+                  type   : 'empty',
+                  prompt : 'First name must not be empty'
+                }
+              ]
+            },
             email: {
               identifier  : 'email',
               rules: [
-                {
-                  type   : 'empty',
-                  prompt : 'Please enter your e-mail'
-                },
                 {
                   type   : 'email',
                   prompt : 'Please enter a valid e-mail'
                 }
               ]
             },
+            username: {
+              identifier  : 'username',
+              rules : [
+                {
+                  type   : 'length[3]',
+                  prompt : 'Username must be at least 3 characters'
+                }
+              ]
+            },
             password: {
-              identifier  : 'password',
+              identifier  : 'password_1',
               rules: [
                 {
-                  type   : 'empty',
-                  prompt : 'Please enter your password'
-                },
+                  type   : 'length[8]',
+                  prompt : 'Password must be at least 8 characters'
+                }
+              ]
+            },
+            passwordConfirm: {
+              identifier : 'password_2',
+              rules: [
                 {
-                  type   : 'length[6]',
-                  prompt : 'Your password must be at least 6 characters'
+                  type   : 'match[password_1]',
+                  prompt : 'Password does not match'
                 }
               ]
             }
