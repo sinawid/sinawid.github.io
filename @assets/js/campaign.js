f2294689c51e5bc5e7c8209ad132ef53014dc351
harrylin88 <harryprabowo10@gmail.com>
2018-06-10 19:05:39 +0700

    Cards update

--- @assets/js/campaign.js
+++ @assets/js/campaign.js
@@ -1,5 +1,23 @@
 $(document)
     .ready(function() {
-			
+				$('.special.cards .image').dimmer({
+				  on: 'hover'
+				});
+
+			// semantic documentation on progres bars: https://semantic-ui.com/modules/progress.html#/usage
+				$('#card .ui.progress').progress(
+					'set percent', 50
+				);
+
+				$('#card .ui.progress')
+				  .progress({
+						total: 100000,
+						text: {
+				      active  : 'IDR{value} dari IDR{total} telah didanai',
+				      success : 'CAMPAIGN SELESAI'
+				    }
+					}
+				);
+
 	})
 ;
